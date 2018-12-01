b80a6bbe9b343bb15dbb007c08a23649565c5210
Katrina Marie Smith <smit3285@purdue.edu>
2018-11-30 23:19:10 -0500

    drivergui

--- src/driveshare/CreateAccount.java
+++ src/driveshare/CreateAccount.java
@@ -49,7 +49,7 @@ public class CreateAccount extends javax.swing.JFrame {
         isPassenger = new javax.swing.JRadioButton();
         jLabel6 = new javax.swing.JLabel();
         isDriver = new javax.swing.JRadioButton();
-        state = new javax.swing.JComboBox<>();
+        state = new javax.swing.JComboBox<String>();
         jLabel1 = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         licenseNumber = new javax.swing.JTextField();
@@ -104,7 +104,7 @@ public class CreateAccount extends javax.swing.JFrame {
             }
         });
 
-        state.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Flora", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming" }));
+        state.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Flora", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming" }));
 
         jLabel1.setText("First Name");
 
